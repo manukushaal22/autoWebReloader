6af1765a427135820c8108ce20a770d741fc9806
kushaalhulsoor <kushaal.hulsoor@cleartrip.com>
2020-04-29 15:40:31 +0530

    added logs

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -24,18 +26,30 @@ def generateEnvironmentSpecificURL(constants):
         url += '.exe'
     return url
 
+def log(data):
+    return "["+str(datetime.now())+"]" + " INFO "+data
+
 if __name__=="__main__":
     constants = Constants()
 
     driver = webdriver.Chrome(generateEnvironmentSpecificURL(constants))
+    print(log("Driver Loaded"))
     driver.get(constants.WEBSITE_URL)
+    print(log("Redirected to url: " + constants.WEBSITE_URL))
     time.sleep(2)
     driver.find_element_by_name(constants.USERNAME_ELEMENT_NAME).send_keys(constants.USERNAME)
+    print(log("Username Entered: " + constants.USERNAME))
     time.sleep(2)
     driver.find_element_by_name(constants.PASSWORD_ELEMENT_NAME).send_keys(constants.PASSWORD)
+    print(log("Password Entered."))
     time.sleep(2)
     driver.find_element_by_id(constants.LOGINBUTTON_ELEMENT_ID).send_keys(Keys.ENTER)
+    print(log("Logged In."))
     time.sleep(2)
+    reload_count = 0
     while True:
         time.sleep(constants.RELOAD_INTERVAL_SECONDS)
         driver.refresh()
+        reload_count+=1
+        print(log("Reloaded page for "+str(reload_count)+" time(s) now with interval: "+str(constants.RELOAD_INTERVAL_SECONDS)+" seconds."))
+
